5dcb76bbf2b93012d5fdb484a3a436d329adbdd2
Justmakinmusic <mjstemen45@gmail.com>
2013-08-05 09:26:46 -0500

    does not break bedrock

--- jmm/mods/Diamerald/blackDiameraldpickaxe.java
+++ jmm/mods/Diamerald/blackDiameraldpickaxe.java
@@ -38,26 +38,6 @@ public class blackDiameraldpickaxe extends ItemPickaxe {
 		par1ItemStack.addEnchantment(Enchantment.fortune, 3);
 	}
 
-	public boolean canHarvestBlock(Block par1Block) {
-		return par1Block == Block.obsidian ? this.toolMaterial
-				.getHarvestLevel() == 3
-				: (par1Block != Block.blockDiamond
-						&& par1Block != Block.oreDiamond ? (par1Block != Block.oreEmerald
-						&& par1Block != Block.blockEmerald ? (par1Block != Block.blockGold
-						&& par1Block != Block.oreGold ? (par1Block != Block.blockIron
-						&& par1Block != Block.oreIron ? (par1Block != Block.blockLapis
-						&& par1Block != Block.oreLapis ? (par1Block != Block.oreRedstone
-						&& par1Block != Block.oreRedstoneGlowing ? (par1Block.blockMaterial == Material.rock ? true
-						: (par1Block.blockMaterial == Material.iron ? true
-								: par1Block.blockMaterial == Material.anvil))
-						: this.toolMaterial.getHarvestLevel() >= 2)
-						: this.toolMaterial.getHarvestLevel() >= 1)
-						: this.toolMaterial.getHarvestLevel() >= 1)
-						: this.toolMaterial.getHarvestLevel() >= 2)
-						: this.toolMaterial.getHarvestLevel() >= 2)
-						: this.toolMaterial.getHarvestLevel() >= 2);
-
-	}
 
 	public boolean onBlockDestroyed(ItemStack par1ItemStack, World par2World,
 			int par3, int par4, int par5, int par6,
