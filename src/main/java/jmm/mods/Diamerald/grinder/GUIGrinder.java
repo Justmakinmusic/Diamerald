e9b6224fc740a7de9882b2748d61ad9a56eff906
Justmakinmusic <mjstemen45@gmail.com>
2014-03-17 11:01:17 -0500

    Gui changes

--- src/main/java/jmm/mods/Diamerald/grinder/GUIGrinder.java
+++ src/main/java/jmm/mods/Diamerald/grinder/GUIGrinder.java
@@ -1,8 +1,7 @@
 package jmm.mods.Diamerald.grinder;
 
 import jmm.mods.Diamerald.Diamerald;
-import jmm.mods.Diamerald.PacketHandler.PacketGrinder;
-import jmm.mods.Diamerald.grinder.ContainerGrinder;
+import jmm.mods.Diamerald.packethandler.PacketGrinder;
 import jmm.mods.Diamerald.tileentity.TileEntityGrinder;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.resources.I18n;
@@ -14,8 +13,8 @@ import org.lwjgl.opengl.GL11;
 
 public class GUIGrinder extends GuiContainer
 {
-	private static final ResourceLocation grinderGuiTextures = new ResourceLocation("diamerald", "textures/gui/grinder.png");
-    private TileEntityGrinder tileGrinder;
+	private static final ResourceLocation grinderGuiTextures = new ResourceLocation("diamerald", "textures/gui/grindergui.png");
+    public TileEntityGrinder tileGrinder;
     
 
     public GUIGrinder(InventoryPlayer par1InventoryPlayer, TileEntityGrinder par2TileEntityGrinder)
@@ -24,15 +23,14 @@ public class GUIGrinder extends GuiContainer
         this.tileGrinder = par2TileEntityGrinder;
     }
     
-    
-
     /**
      * Draw the foreground layer for the GuiContainer (everything in front of the items)
      */
     protected void drawGuiContainerForegroundLayer(int par1, int par2)
     {
         String s = this.tileGrinder.hasCustomInventoryName() ? this.tileGrinder.getInventoryName() : I18n.format(this.tileGrinder.getInventoryName(), new Object[0]);
-        this.fontRendererObj.drawString(s, this.xSize / 2 - this.fontRendererObj.getStringWidth(s) / 2, 6, 4210752);
+        this.fontRendererObj.drawString(I18n.format("Grinder", new Object[0]), this.xSize / 2 - this.fontRendererObj.getStringWidth("Grinder") / 2, 6, 4210752);
+        this.fontRendererObj.drawString(s, this.xSize / 2 - this.fontRendererObj.getStringWidth(s) / 2, 15, 4210752);
         this.fontRendererObj.drawString(I18n.format("container.inventory", new Object[0]), 8, this.ySize - 96 + 2, 4210752);
     }
 
@@ -45,21 +43,22 @@ public class GUIGrinder extends GuiContainer
         this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
         int i1;
         
-        if (this.tileGrinder.canSmelt())
+        if (this.tileGrinder.isGrinding())
         {
             i1 = this.tileGrinder.getItemTimeScaled(12);
-            this.drawTexturedModalRect(k + 62, l + 52 + 12 - i1, 176, 11 - i1, 4, i1 + 2);
+            this.drawTexturedModalRect(k + 62, l + 52 + 12 - i1, 176, 11 - i1, 16, i1 + 2);
         }
       
         if (this.tileGrinder.isBurning())
         {
-        	 i1 = this.tileGrinder.getBurnTimeRemainingScaled(12);
-             this.drawTexturedModalRect(k + 14, l + 18 + 13 - i1, 176, 13 - i1, 4, i1 + 1); // i1 + 2);
+        	 i1 = this.tileGrinder.getBurnTimeRemainingScaled(45);
+             this.drawTexturedModalRect(k + 10, l + 36 + 13 - i1, 176, 93 - i1, 12, i1 + 2); // i1 + 2);
+             //Diamerald.packetPipeline.sendToAll(new PacketGrinder(tileGrinder.grinderBurnTime, tileGrinder.xCoord, tileGrinder.yCoord, tileGrinder.zCoord));
         }
 
         i1 = this.tileGrinder.getCookProgressScaled(24);
         this.drawTexturedModalRect(k + 79, l + 34, 176, 14, i1 + 1, 16);
         
-        Diamerald.packetPipeline.sendToServer(new PacketGrinder(tileGrinder.grinderBurnTime, tileGrinder.xCoord, tileGrinder.yCoord, tileGrinder.zCoord));
+        //Diamerald.packetPipeline.sendToServer(new PacketGrinder(tileGrinder.grinderBurnTime, tileGrinder.xCoord, tileGrinder.yCoord, tileGrinder.zCoord));
     }
 }
